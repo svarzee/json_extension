44e08a95a7a103a625f645a883e9bf8769bdcd15
svarzee <svarzee@gmail.com>
2019-12-11 11:48:28 +0100

    Use json_annotation

--- json_extension_annotations/CHANGELOG.md
+++ json_extension_annotations/CHANGELOG.md
@@ -1,2 +1,5 @@
+## 0.2.0
+* Use json_annotation
+
 ## 0.1.0
 * Initialize package
--- json_extension_annotations/lib/annotations.dart
+++ json_extension_annotations/lib/annotations.dart
@@ -1,5 +1,27 @@
 import 'package:class_extensions_annotations/annotations.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-class Json extends Extension {
-  const Json();
+class Json extends JsonSerializable implements Extension {
+  const Json({
+    bool anyMap,
+    bool checked,
+    bool createFactory,
+    bool createToJson,
+    bool disallowUnrecognizedKeys,
+    bool explicitToJson,
+    FieldRename fieldRename,
+    bool ignoreUnannotated,
+    bool includeIfNull,
+    bool nullable,
+  }) : super(
+            anyMap: anyMap,
+            checked: checked,
+            createFactory: createFactory,
+            createToJson: createToJson,
+            disallowUnrecognizedKeys: disallowUnrecognizedKeys,
+            explicitToJson: explicitToJson,
+            fieldRename: fieldRename,
+            ignoreUnannotated: ignoreUnannotated,
+            includeIfNull: includeIfNull,
+            nullable: nullable);
 }
